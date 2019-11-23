8d45e4b8642cf8605d84b5c102c6af47314b6dcf
jstiven01 <stiven10@gmail.com>
2019-11-22 19:09:10 -0500

    Styling home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class StaticPagesController < ApplicationController
-  def home; end
+  def home
+    @events_upcoming = Event.upcoming
+    @events_past = Event.past
+  end
 end
